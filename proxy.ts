b982b9facbf42467f587b9b82553bc7ddee838a7
mutaqinmi <mutaqinmi2586@gmail.com>
2025-11-17 10:33:25 +0700

    fix: proxy redirection

--- proxy.ts
+++ proxy.ts
@@ -7,33 +7,36 @@ export default async function proxy(req: NextRequest){
     }
 
     const sessionToken = req.cookies.get("session_token")?.value;
-    if (!sessionToken){
-        return NextResponse.json({ 
-            message: "Unauthorized"
-        }, { status: 401 });
-    }
-    
-    const verifiedToken = verifyToken(sessionToken);
-    if (!verifiedToken){
-        return NextResponse.json({ 
-            message: "Unauthorized"
-        }, { status: 401 });
-    }
+    const verifiedToken = sessionToken ? verifyToken(sessionToken) : false;
 
     const authenticated = sessionToken && verifiedToken;
+    const authPages = [
+        "/signin",
+        "/signup"
+    ];
 
-    if (authenticated && (req.nextUrl.pathname.startsWith("/signin") || req.nextUrl.pathname.startsWith("/signup"))){
-        return NextResponse.redirect(new URL("/", req.url));
-    } else if (!authenticated && !req.nextUrl.pathname.startsWith("/signin") && !req.nextUrl.pathname.startsWith("/signup")) {
-        return NextResponse.redirect(new URL("/signin", req.url));
-    }
+    if (authenticated){
+        if (req.nextUrl.pathname.startsWith("/") && authPages.includes(req.nextUrl.pathname)){
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    } else if (!authenticated){
+        if (req.nextUrl.pathname.startsWith("/") && !authPages.includes(req.nextUrl.pathname)){
+            return NextResponse.redirect(new URL("/signin", req.url));
+        } else if (req.nextUrl.pathname.startsWith("/") && authPages.includes(req.nextUrl.pathname)){
+            return NextResponse.next();
+        }
 
-    if(!authenticated && req.nextUrl.pathname.startsWith("/api")){
+        if (req.nextUrl.pathname.startsWith("/") && req.nextUrl.pathname.startsWith("/api/transactions")){
+            return NextResponse.json({
+                message: "Unauthorized"
+            }, { status: 401 } );
+        }
+
         return NextResponse.json({ 
             message: "Unauthorized"
         }, { status: 401 });
     }
-    
+
     return NextResponse.next();
 }
 
